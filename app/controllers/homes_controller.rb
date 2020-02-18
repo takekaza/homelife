d8a64d60b7f4bde2cef1061a2d28576723d3e81f
takekaza <58417359+takekaza@users.noreply.github.com>
2020-02-18 13:04:12 +0900

    Update homes_controller.rb

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,10 @@
 class HomesController < ApplicationController
   before_action :set_home, except: [:indexes, :new, :create, :search]
 
+  def index
+
+  end
+  
   def indexes
     @home = Home.includes(:user)
     @images = Image.all
